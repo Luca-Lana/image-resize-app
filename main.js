46b983903a9ddd8a3b87fa4e98c1d61576e3b612
Luca <lucalana13@gmail.com>
2023-01-23 17:49:58 -0300

    organizing the project

--- main.js
+++ main.js
@@ -1,14 +1,20 @@
 const { app, BrowserWindow } = require('electron')
 const path = require('path')
 
+const isDev = (process.env.NODE_ENV !== 'production')
 const isMac = (process.platform === 'darwin')
 
 function createMainWindow() {
   const mainWindow = new BrowserWindow({
     title: "Image Resizer",
-    width: 500,
+    width: isDev ? 1000 : 500,
     height: 600,
   })
+
+  //Open dev tools if in dev env
+  if (isDev) {
+    mainWindow.webContents.openDevTools()
+  }
 
   mainWindow.loadFile(path.join(__dirname, './renderer/index.html'))
 }
@@ -16,7 +22,7 @@ function createMainWindow() {
 
 app.whenReady().then(() => {
   createMainWindow()
-  
+
   app.on('activate', () => {
     if (BrowserWindow.getAllWindows().length === 0) {
       createMainWindow()
