7107e8a580cd535acd3aec9db38b78691aff7bb6
Luca <lucalana13@gmail.com>
2023-01-23 18:39:23 -0300

    configuration menu

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, Menu } = require('electron')
 const path = require('path')
 
 const isDev = (process.env.NODE_ENV !== 'production')
 const isMac = (process.platform === 'darwin')
 
+//Creating the main window
 function createMainWindow() {
   const mainWindow = new BrowserWindow({
     title: "Image Resizer",
@@ -18,10 +19,24 @@ function createMainWindow() {
 
   mainWindow.loadFile(path.join(__dirname, './renderer/index.html'))
 }
+//Create about window
+function createAboutWindow() {
+  const aboutWindow = new BrowserWindow({
+    title: "About Image Resizer",
+    width: 300,
+    height: 300,
+  })
 
+  aboutWindow.loadFile(path.join(__dirname, './renderer/about.html'))
+}
 
+// App is ready
 app.whenReady().then(() => {
   createMainWindow()
+
+  //Implements menu
+  const mainMenu = Menu.buildFromTemplate(menu)
+  Menu.setApplicationMenu(mainMenu)
 
   app.on('activate', () => {
     if (BrowserWindow.getAllWindows().length === 0) {
@@ -30,6 +45,21 @@ app.whenReady().then(() => {
   })
 })
 
+//Menu template
+const menu = [
+  {
+    role: 'fileMenu'
+  },
+  {
+    label: 'Help',
+    submenu: [
+      {
+        label: 'About',
+        click: createAboutWindow
+      }
+    ]
+  }
+]
 
 app.on('window-all-close', () => {
   if (!isMac) {
